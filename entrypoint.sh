dd7eb238056cf9b482757631cec84f1418446647
srdsedfrderw <99366582+srdsedfrderw@users.noreply.github.com>
2022-09-21 14:30:25 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/s233"
+  Vless_Path="/edf1088a-b884-48bf-9c83-d10d07dfafd0-vless"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="273d7707-9bb1-44ff-b24c-b59876344cac"
+  Vless_UUID="edf1088a-b884-48bf-9c83-d10d07dfafd0"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/s244"
+  Vmess_Path="/98b97b23-c32c-44a9-8e42-915663bac37b-vmess"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="273d7707-9bb1-44ff-b24c-b59876344cac"
+  Vmess_UUID="edf1088a-b884-48bf-9c83-d10d07dfafd0"
 fi
 echo ${Vmess_UUID}
 
